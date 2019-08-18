8784fc06a29daf94d70f68a112292c2285e9f484
smeenkere <sandeepmeenkere@gmail.com>
2019-08-18 23:04:42 +0530

    Update argmt.sh
    Add updated script

--- argmt.sh
+++ argmt.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-echo "Script Name: $0"
-function func {
-    for var in $*
-    do
-        let i=i+1
-        echo "The \$${i} argument is: ${var}"
-    done
-    echo "Total count of arguments: $#"
-}
-func We are argument variable Added
+sum=$(( $1 + $2 ))
+ 
+echo "Sum is: $sum" 
 
